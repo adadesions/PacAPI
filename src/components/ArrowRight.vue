beb03608a9b5a226fa1af4add57d0e4f186ce5fe
adadesions <adadesions@gmail.com>
2020-05-21 02:34:35 +0700

    Adjust for large screen

--- src/components/ArrowRight.vue
+++ src/components/ArrowRight.vue
@@ -1,8 +1,7 @@
 <template>
   <q-icon
+    class="arrowIcon"
     name="keyboard_arrow_right"
-    size="lg"
-    style="color:#FFF; margin-top:-0.7em"
   ></q-icon>
 </template>
 
@@ -11,3 +10,17 @@ export default {
   name: 'ArrowRight',
 };
 </script>
+
+<style lang="sass" scoped>
+  @media (max-width: $breakpoint-lg-max)
+    .arrowIcon
+      font-size: 6em
+      color: #FFF
+      margin-top: -0.9em
+
+  @media (max-width: $breakpoint-sm-max)
+    .arrowIcon
+      font-size: 3em
+      color: #FFF
+      margin-top: -0.6em
+</style>
--- src/components/EnergyBattery.vue
+++ src/components/EnergyBattery.vue
@@ -12,67 +12,145 @@
 export default {
   name: 'EnergyBattery',
   props: ['percentage'],
+  data() {
+    return {
+      fullEnergySm: 168,
+      fullEnergyLg: 410,
+    }
+  },
   computed: {
     energyLevel() {
-      return `width: ${(163 * this.percentage) / 100}px`
-    },
-    energyTextAdjust() {
-      let result = 530
-      if (this.percentage < 10) {
-        result += 20
-      } else if (this.percentage < 100) {
-        result += 15
+      let fullEnergyScale = 0
+      const screenWidth = window.innerWidth
+
+      if (screenWidth <= 800) {
+        fullEnergyScale = this.fullEnergySm
+      } else {
+        fullEnergyScale = this.fullEnergyLg
       }
 
-      return `left: ${result}px`
+      return `width: ${(fullEnergyScale * this.percentage) / 100}px`
+    },
+    energyTextAdjust() {
+      const screenWidth = window.innerWidth
+      let data = []
+
+      if (screenWidth <= 800) {
+        data = [11.7, 0.7, 0.5]
+      } else {
+        data = [11.5, 0.7, 0.4]
+      }
+
+      let result = data[0]
+      if (this.percentage >= 0 && this.percentage < 10) {
+        result += data[1]
+      } else if (this.percentage < 100) {
+        result += data[2]
+      }
+
+      return `left: ${result}em`
     },
   },
 }
 </script>
 
 <style lang="sass" scope>
+  // Sm-screen
+  $full-energy-sm: 163px
   $batt-left: 500px
 
-  .app-battery
-    position: absolute
-    width: 200px
-    margin-top: 1em
-    left: $batt-left - 12px
+  // Lg-screen
+  $energy-height: 9em
+  $energy-top: 14em
+  $batt-left-lg: 67em
 
-  .energy
-    position: absolute
-    top: 108px
-    left: $batt-left
-    height: 50px
-    background-color: $theme-orange
-    border-radius: 5px
+  @media (max-width: $breakpoint-lg-max)
+    .app-battery
+      position: absolute
+      width: 35em
+      margin-top: 1.5em
+      left: 65em
 
-  .energy-background
-    position: absolute
-    top: 108px
-    left: $batt-left
-    height: 50px
-    width: 163px
-    background-color: $theme-orange
-    border-radius: 5px
-    opacity: 0.3
+    .energy
+      position: absolute
+      top: $energy-top
+      left: $batt-left-lg
+      height: $energy-height
+      background-color: $theme-orange
+      border-radius: 5px
 
-  .energy-text
-    position: absolute
-    top: 100px
-    font-family: 'Prompt'
-    font-size: 45px
-    font-weight: bold
-    color: $theme-white
+    .energy-background
+      position: absolute
+      top: 108px
+      left: $batt-left
+      height: 50px
+      width: 163px
+      background-color: $theme-orange
+      border-radius: 5px
+      opacity: 0.3
 
-  .text-battery
-    position: absolute
-    top: 170px
-    left: $batt-left - 10px
-    font-size: 15px
-    color: $theme-orange
-    font-family: 'Prompt'
-    font-weight: bold
-    letter-spacing: 2px
-    padding-left: 2.3em
+    .energy-text
+      position: absolute
+      top: 2.1em
+      left: 10em
+      font-family: 'Prompt'
+      font-size: 90px
+      font-weight: bold
+      color: $theme-white
+
+    .text-battery
+      position: absolute
+      top: 170px
+      left: $batt-left - 10px
+      font-size: 15px
+      color: $theme-orange
+      font-family: 'Prompt'
+      font-weight: bold
+      letter-spacing: 2px
+      padding-left: 2.3em
+
+
+  @media (max-width: $breakpoint-sm-max)
+    .app-battery
+      position: absolute
+      width: 200px
+      margin-top: 1em
+      left: $batt-left - 12px
+
+    .energy
+      position: absolute
+      top: 108px
+      left: $batt-left
+      height: 50px
+      background-color: $theme-orange
+      border-radius: 5px
+
+    .energy-background
+      position: absolute
+      top: 108px
+      left: $batt-left
+      height: 50px
+      width: 163px
+      background-color: $theme-orange
+      border-radius: 5px
+      opacity: 0.3
+
+    .energy-text
+      position: absolute
+      top: 100px
+      font-family: 'Prompt'
+      font-size: 45px
+      font-weight: bold
+      color: $theme-white
+
+    .text-battery
+      position: absolute
+      top: 170px
+      left: $batt-left - 10px
+      font-size: 15px
+      color: $theme-orange
+      font-family: 'Prompt'
+      font-weight: bold
+      letter-spacing: 2px
+      padding-left: 2.3em
 </style>
